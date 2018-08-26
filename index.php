5543455281ecbb1ac29bd2eb3ff17b1cd4e2383e
Monty2107 <monterrosadelgado@gmail.com>
2018-08-25 22:22:27 -0600

    Diseño base

---
+++ index.php
@@ -0,0 +1,15 @@
+//Aqui se definira el inicio de session para ingresar al sistema.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="produccion/inicio.php">
+    <input type="submit" value="Entrar" />
+</form>
+</body>
+</html>
---
+++ produccion/inicio.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Ues-Online | Cojutepeque</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="../public/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME ICONS  -->
+    <link href="../public/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="../public/css/style.css" rel="stylesheet" />
+    <!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- <link href="../build/css/custom.css" rel="stylesheet"> -->
+</head>
+
+<body>
+
+    <!-- HEADER END-->
+    <!-- <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container">
+            <div class="navbar-header">
+            <img src="../public/images/logo1.png" width="80" height="80"/>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src="../public/images/user.png" alt="">Monterrosa
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="javascript:;">Mi Perfil</a></li>
+                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i>Cerrar Session</a></li>
+                  </ul>
+                </li>
+    </lu>
+            </div>
+        </div>
+    </div> -->
+     <!-- top navigation -->
+     <div class="top_nav">
+          <div class="navbar navbar-inverse set-radius-zero">
+          <div class="container">
+            <div class="navbar-header">
+            <img src="../public/images/logo1.png" width="80" height="80"/>
+            </div>
+            <nav>
+              <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src="../public/images/user.png" alt="">Monterrosa
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="javascript:;">Mi Perfil</a></li>
+                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i>Cerrar Session</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+        </div>
+          </div>
+        </div>
+        <!-- /top navigation -->
+    <!-- LOGO HEADER END-->
+    <!-- <section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a class="menu-top-active" href="ui.html">Inicio</a></li>
+                            <li><a href="blank.html">Administrativo</a></li>
+                            <li><a href="blank.html">Biblioteca</a></li>
+                            <li><a href="table.html">Mi Perfil</a></li>
+                            <li><a href="login.html">Cerrar Session</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section> -->
+    <!-- MENU SECTION END-->
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="page-head-line">Menu Principal.</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                    <div class="dashboard-div-wrapper bk-clr-two">
+                      <a href="Administracion/inicioAdmin.php" aria-expanded="false">
+                        <i class="fa fa-edit dashboard-div-icon"></i>
+                        </a>
+                        <h5>Administrativo.</h5>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                    <div class="dashboard-div-wrapper bk-clr-two">
+                        <i class="fa fa-edit dashboard-div-icon"></i>
+                        <h5>Biblioteca.</h5>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                    <div class="dashboard-div-wrapper bk-clr-two">
+                        <i class="fa fa-cogs dashboard-div-icon"></i>
+                        <h5>Activo Fijo.</h5>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                    <div class="dashboard-div-wrapper bk-clr-two">
+                        <i class="fa fa-bell-o dashboard-div-icon"></i>
+                        <h5>Informacion.</h5>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- CONTENT-WRAPPER SECTION END-->
+    <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+          &copy; Universidad de El Salvador 2018.
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+    <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY SCRIPTS -->
+    <script src="../public/js/jquery-1.11.1.js">
+    </script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="../public/js/bootstrap.js">
+    </script>
+</body>
+
+</html>
