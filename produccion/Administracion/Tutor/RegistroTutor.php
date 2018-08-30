9fdc9359c332362c631853c6e2214c8165e89ff5
willian92 <willianernesto_92@hotmail.com>
2018-08-30 15:06:47 -0600

    RegistroTutorr

--- produccion/Administracion/Tutor/RegistroTutor.php
+++ produccion/Administracion/Tutor/RegistroTutor.php
@@ -230,13 +230,23 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2><small>Registro Tutor</small></h2>
+                    <h2><small>Registro de Personal</small></h2>
                    
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
                     <br />
                     <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Cargo</label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <select class="form-control">
+                            <option>Tutor</option>
+                            <option>Tecnico</option>
+                          </select>
+                        </div>
+                        </div>
 
                     <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="code-name">Codigo <span class="required">*</span>
