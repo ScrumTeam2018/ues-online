77cd3d8b1a14a4249f9841b5cb48caae0d192a08
magda16 <magdacordova2@gmail.com>
2018-08-30 15:07:46 -0600

    vistaregistroplan

---
+++ produccion/Administracion/planestudio/registroplan.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- abrir head el cierre esta dentro del archivo que se incluye -->
+<head>
+<!-- estilo vertical para las ventanas -->
+<style>
+* {box-sizing: border-box}
+body {font-family: "Lato", sans-serif;}
+
+/* Style the tab */
+.tab {
+    float: left;
+    border: 1px solid #ccc;
+    background-color: #f1f1f1;
+    width: 10%;
+    height: 300px;
+}
+
+/* Style the buttons inside the tab */
+.tab button {
+    display: block;
+    background-color: inherit;
+    color: black;
+    padding: 22px 16px;
+    width: 100%;
+    border: none;
+    outline: none;
+    text-align: left;
+    cursor: pointer;
+    font-size: 17px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+    background-color: #ccc;
+}
+
+/* Create an active/current "tab button" class */
+.tab button.active {
+    background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+    float: left;
+    padding: 0px 12px;
+    border: 1px solid #ccc;
+    width: 90%;
+    border-left: none;
+    height: 300px;
+    display: none;
+}
+
+/* Clear floats after the tab */
+.clearfix::after {
+    content: "";
+    clear: both;
+    display: table;
+}
+</style>
+<?php include '../../global/head.php' ?>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="#" class="site_title"><img src="../../../public/images/logo2.png"/><span> Sede Cojutepeque</span></a>
+            </div>
+            <div class="clearfix"></div>
+            <!-- menu profile quick info 
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="../public/images/descarga.png" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Bienvenido,</span>
+                <h2>John Doe</h2>
+              </div>
+            </div>
+          /menu profile quick info -->
+            <br />
+            <?php include '../../global/menu.php' ?>
+          </div>
+        </div>
+       <?php include '../../global/navigation.php' ?>
+        <!-- page content Panel de Trabajo -->
+        <div class="right_col" role="main">
+        <!--Monty: Aqui dentro iria todo lo necesario para el panel de trabajo -->
+
+        <!--Magda titulo de plan -->
+        <div class="page-title">
+              <div class="title_left">
+                <h3>Plan de Estudio </h3>
+                
+              </div> 
+        </div>
+        <div class="clearfix"></div>
+
+        <div class="row" >
+              <div class="col-md-8 col-sm-8 col-xs-8">
+                <div class="x_panel" >
+                  <div class="x_title">
+                    <h4>Registro</h4>
+                    <ul class="nav navbar-right panel_toolbox">
+                     
+                    <li><a href="registroasignatura.php">Registrar Asignaturas</a>
+                    </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nombre: <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Año: <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Cantidad de Ciclos: <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Carrera: <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <select name="transporte">
+                            <option>Ingenieria en Sistemas</option>
+                            <option>Administracion</option>
+                            </select>
+                        </div>
+                      </div>
+
+                     
+                      
+                      
+                      <div class="ln_solid"></div>
+                      <div class="form-group" align="right">
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                          <button class="btn btn-round btn-primary" type="button"><i class="fa fa-save">  Guardar</i></button>
+						  <button class="btn btn-round btn-default" type="reset"><i class="fa fa-ban">  Cancelar</i></button>
+                        </div>
+                      </div>
+
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+        
+
+
+
+        </div>
+        <!-- /page content -->    
+      </div>
+    </div>
+    <?php include '../../global/footer.php' ?>
+    <?php include '../../global/script.php' ?>
+    
+  </body>
+</html>
