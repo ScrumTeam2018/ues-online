1fcae3734712fd597e82cd62297d0cee1a51efde
Monty2107 <monterrosadelgado@gmail.com>
2018-09-05 18:13:33 -0600

    generador de formulario plan de estudio usando jquery

--- produccion/Administracion/planestudio/registroasignatura.php
+++ produccion/Administracion/planestudio/registroasignatura.php
@@ -58,6 +58,7 @@
   <button class="tablinks" onmouseover="openCiclo(event, '3')">ciclo 5</button>
 </div>
 
+
 <div id="1" class="tabcontent">
               <div class="x_title">
                     <h4>Registro Asignatura Ciclo 1</h4>                
@@ -73,9 +74,11 @@
                       </div>
                       <div class="clearfix"></div>
                       <br><br>
-
+                    <!-- Monty: comienzo del form -->
                     <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
-                    <div id="add" class="add"></div>
+                      <!-- Monty: donde se agregaran los nuevos formularios -->
+                      
+                    <div id="addFormulario" class="addFormulario"></div>
 
                       <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Codigo: <span class="required"></span>
@@ -114,7 +117,7 @@
                           </select>
                         </div>
                       </div>
-
+                    <!-- fin de formulario -->
                     </form>
                   </div>
                   
@@ -123,7 +126,7 @@
 
 </div>
 
-
+<!-- agregar la parte derecha por cada panel -->
 <div id="2" class="tabcontent">
   <h3>ciclo 2</h3>
   <p>formulario 2</p> 
@@ -133,16 +136,23 @@
   <h3>ciclo 3</h3>
   <p>formulario 3</p>
 </div>
+<!-- Monty: formulario pa el resguardar la informacion -->
+<form action="">
+<!-- Monty: agregara todos los tabcontent dinamicamente aqui se insertaran para el llamado -->
+<div id= "addtabcontent" class="addtabcontent" >
+</div>
+</form>
 
         </div>
         <!-- /page content -->    
         <?php include '../../global/footer.php' ?>
       </div>
     </div>
-    
+    <!-- llamado de los script a importar -->
     <?php include '../../global/script.php' ?>
 
     <script>
+      /*Monty: metodo que hace el paso entre los ciclos en el tab.. */
 function openCiclo(evt, id) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -157,25 +167,50 @@ function openCiclo(evt, id) {
     evt.currentTarget.className += " active";
 }
 
-function getInput(type, placeholder){
-  var nodo = document.createElement("input");
-  nodo.type = type;
-  nodo.placeholder = placeholder;
-  return nodo;
-}
 
-function append(className, nodoToAppend){
-  var nodo = document.getElementsByClassName(className)[0];
-  nodo.appendChild(nodoToAppend);
-}
-
-/*ESTE ES EL CODIGO Q SE SUPONE DEBERIA  AGREGAR LAS CAJAS DE TEXTO */
+/*Monty: codigo que genera los nuevo formularios en el plan de estudio usando JQuery */
 function Agregaitems(){
-
-	var nodo1 = getInput("text", "Nombre del producto NUEVO");
-    append("add", nodo1);
-
+  /* llamo al div donde deseo inserta el formulario*/
+var dir = $('#addFormulario');
+/* hago el string que deseo insertar en el div que he llamado */
+ var formulario= "<div class='form-group'>"+
+ "<label class='control-label col-md-3 col-sm-3 col-xs-12' for='first-name'>"+
+ "Codigo: <span class='required'></span>"+
+ "</label><div class='col-md-6 col-sm-6 col-xs-12'>"+
+ "<input type='text' id='first-name' required='required' class='form-control col-md-7 col-xs-12'>"+
+ "</div>"+
+ "</div>"+
+ "<div class='form-group'>"+
+ "<label class='control-label col-md-3 col-sm-3 col-xs-12' for='last-name'>Nombre: <span class='required'></span>"+
+ "</label>"+
+ "<div class='col-md-6 col-sm-6 col-xs-12'>"+
+ "<input type='text' id='last-name' name='last-name' required='required' class='form-control col-md-7 col-xs-12'>"+
+ "</div>"+
+ "</div>"+
+ "<div class='form-group'>"+
+ "<label class='col-md-3 col-sm-3 col-xs-12 control-label'>Tipo:</label>"+
+ "<div class='radio'>"+
+ "<label>"+
+ "<input type='radio' class='flat' name='iCheck'> Obligatoria"+
+ "</label>"+
+ "<label>"+
+ "<input type='radio' class='flat' name='iCheck'> Electiva"+
+ "</label>"+
+ "</div>"+
+ "</div>"+
+ "<div class='form-group'>"+
+ "<label class='control-label col-md-3 col-sm-3 col-xs-12'>Unidades Valorativas: </label>"+
+ "<div class='col-md-6 col-sm-6 col-xs-12'>"+
+ "<select class='form-control'>"+
+ "<option>2 </option>"+
+ "<option>4 </option>"+
+ "</select>"+
+ "</div>"+
+ "</div>";
+ /* llamo al div y le digo con el metodo append que ahi dentro debe agregarlo todo que es
+ escribi en la variable formulario*/
+ dir.append(formulario);
 	}
 </script>
-  </body>
+</body>
 </html>
